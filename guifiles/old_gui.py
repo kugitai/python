f5aa86b33cd1fbb4d0eeb7a8cc91a815d2531f4d
kugitai <98938330+kugitai@users.noreply.github.com>
2023-12-06 14:04:34 +0900

    例外処理の追加

--- guifiles/old_gui.py
+++ guifiles/old_gui.py
@@ -5,8 +5,7 @@ layout = [[sg.Text(key='output')],
           [sg.Input(key='berthyear')],
           [sg.Button('年齢確認')]]
 window = sg.Window('年齢',layout)
-dt_now = datetime.datetime.now()
-year_now = dt_now.year
+dt_now = datetime.datetime.now().year
 while True:
     event,value = window.read()
     if event == sg.WINDOW_CLOSED:
@@ -14,9 +13,10 @@ while True:
     if value['berthyear'] == '':
         window['output'].update('生まれた年を入力して下さい')
         continue
-    if value['berthyear'] < 0:
+    if int(value['berthyear']) <= 0:
         window['output'].update('正の値をいれてください')
-    old = year_now - int(value['berthyear'])
+        continue
+    old = dt_now - int(value['berthyear'])
     if old < 0:
         window['output'].update('現在の年数を超える数値は入れないでください')
         continue
