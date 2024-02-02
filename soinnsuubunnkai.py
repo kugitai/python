8898cc4131254f350676477790863a307902b130
kugitai <98938330+kugitai@users.noreply.github.com>
2024-02-02 09:29:27 +0900

    素因数分解のコードを作成

---
+++ soinnsuubunnkai.py
@@ -0,0 +1,26 @@
+num = int(input())
+sosuu_l = []
+kekka_l = []
+x = 0
+if num <= 1:
+    print("その数は素因数分解できません")
+    quit()
+for i in range(2,num+1):
+    for j in range(2,i):
+        if i % j == 0:
+            break
+    else:
+        sosuu_l.append(i)
+print(sosuu_l)
+
+while True:
+    if num % int(sosuu_l[x]) != 0:
+        x+=1
+    else:
+        num//=int(sosuu_l[x])
+        kekka_l.append(sosuu_l[x])
+        x = 0
+        continue
+    if num == 1:
+        break
+print(kekka_l)
