6a92b349f27c7a7244285a398943e7f0ed9a61d3
kugitai <98938330+kugitai@users.noreply.github.com>
2024-02-28 15:02:11 +0900

    終了できるようにした（バグあり）

--- pi.py
+++ pi.py
@@ -1,14 +1,17 @@
 import random
 while True:
     inpoint = 0
-    allpoint = int(input('何個の点をおきますか'))
-    for i in range(allpoint):
-        x = random.uniform(0,1)
-        y = random.uniform(0,1)
-        if x**2+y**2 <= 1.0:
-            inpoint+=1
-        pi = inpoint*4/allpoint
-    print(pi)
-    if inpoint == 'end':
+    allpoint = input('何個の点をおきますか')
+    if allpoint == 'end':
         break
+    else:
+        allpoint = int(allpoint)
+        for i in range(allpoint):
+            x = random.uniform(0,1)
+            y = random.uniform(0,1)
+            if x**2+y**2 <= 1.0:
+                inpoint+=1
+            pi = inpoint*4/allpoint
+            print(pi)
+
 
