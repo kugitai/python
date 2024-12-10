f19dfa8f0f777e13d3300a4eec3986fd7274211a
kugitai <98938330+kugitai@users.noreply.github.com>
2024-12-10 13:48:48 +0900

    日付表示の追加

--- whether.py
+++ whether.py
@@ -3,6 +3,6 @@ import requests
 url = "https://weather.tsukumijima.net/api/forecast/city/400010"
 
 data = requests.get(url).json()
-print('タイトル：', data['title'])
-print('天気：', data['forecasts'][0]['telop'])
+print(f'{data['publicTimeFormatted']}の{data['title']}')
+print('天気：', data['forecasts'][1]['telop'])
 print('概況：',data['description']['text'])
