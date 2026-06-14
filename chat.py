7390577a29806b3d3d2fade2b70e0479084e8a57
kugitai <98938330+kugitai@users.noreply.github.com>
2026-06-15 00:08:30 +0900

    実行停止の命令に関して
    条件を実行停止をする命令でない時から実行停止を命令された時に変更した

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 name = input('名前を入力してください')
 while True:
     chat = input()
-    if chat != 'quit':
-        print(f'{name}:{chat}')
-    else:
+    if chat == 'quit':
         break
+    else:
+        print(f'{name}:{chat}')
