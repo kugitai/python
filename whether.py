b84887ac6763dac5297116c46f12a419a7810670
kugitai <98938330+kugitai@users.noreply.github.com>
2026-08-14 14:06:08 +0900

    url等の変更
    urlをparamsなしのものに変更した。

--- whether.py
+++ whether.py
@@ -1,8 +1,14 @@
 import requests
 
-url = "https://weather.tsukumijima.net/api/forecast/city/400010"
+url = "https://weather.tsukumijima.net/api/forecast"
 
-data = requests.get(url).json()
-print(f'{data['publicTimeFormatted']}の{data['title']}')
-print('天気：', data['forecasts'][1]['telop'])
+params = {'city':400040}
+
+data = requests.get(url,params=params).json()
+
+print(f'{data['publicTimeFormatted']}現在の{data['title']}予報')
+for i in range(2):
+    print('日付：', data['forecasts'][i]['date'])
+    print('天気：', data['forecasts'][i]['telop'])
+    print(data['forecasts'][i]['image']['url'])
 print('概況：',data['description']['text'])
